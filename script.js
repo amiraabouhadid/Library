8a4de6ae06f0413b8cd138aa42c32286fc0cf558
amira <amiraabouhadid@gmail.com>
2021-06-09 16:28:45 +0200

    Add remove book and change read status functionality

--- script.js
+++ script.js
@@ -29,13 +29,27 @@ function addBookToLibrary(b) {
 function saveToLocalStorage() {
   window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
+
 function index(){
   const table = document.getElementById('booksTable');
+  table.innerHTML =  '<thead><tr><th>Author</th><th>Title</th><th>Pages</th><th>Read</th><th>Remove</th></tr></thead>'   
   myLibrary.forEach((book, i) => {
+
     const data = `<tr><td>${book.author}</td><td>${book.title}</td><td>${book.pages}</td><td><a href="#" onclick="changeStatus(${i});">${book.read}</a></td><th><a id='deleteButton' onclick="removeBook(${i});" href="#" class="btn btn-danger">Remove</a></th></tr>`
+
     table.innerHTML += data;
   });
   saveToLocalStorage();
+}
+
+function changeStatus(i){
+  const book = myLibrary[i];
+  book.read ? book.read = false : book.read = true;
+  index();
+}
+function removeBook(i){
+  myLibrary.splice(i,1);
+  index();
 }
 index();
 document.getElementById('submit-button').addEventListener('click', () => {
