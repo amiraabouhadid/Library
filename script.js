6e0fd10611dd0a5e7ee4d56f133f98476c0001f0
amira <amiraabouhadid@gmail.com>
2021-06-09 17:03:55 +0200

    Run eslint --fix

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const newBookButton = document.querySelector('#newBook');
 const newForm = document.querySelector('#newForm');
 let myLibrary = [];
@@ -16,25 +15,21 @@ function Book(author, title, pages, read) {
   this.title = title;
   this.pages = pages;
   this.read = read;
-  this.info = function(){
-    return `${title} by ${author}, total number of pages: ${pages}`;
-  }
-
 }
 
 function addBookToLibrary(b) {
-  myLibrary.push(b)
+  myLibrary.push(b);
 }
 
 function saveToLocalStorage() {
   window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
-function index(){
+function index() {
   const table = document.getElementById('booksTable');
-  table.innerHTML =  '<thead><tr><th>Author</th><th>Title</th><th>Pages</th><th>Read</th><th>Remove</th></tr></thead>'
+  table.innerHTML = '<thead><tr><th>Author</th><th>Title</th><th>Pages</th><th>Read</th><th>Remove</th></tr></thead>';
   myLibrary.forEach((book, i) => {
-    const data = `<tr><td>${book.author}</td><td>${book.title}</td><td>${book.pages}</td><td><a href="#" onclick="changeStatus(${i});">${book.read}</a></td><th><a id='deleteButton' onclick="removeBook(${i});" href="#" class="btn btn-danger">Remove</a></th></tr>`
+    const data = `<tr><td>${book.author}</td><td>${book.title}</td><td>${book.pages}</td><td><a href="#" onclick="changeStatus(${i});">${book.read}</a></td><th><a id='deleteButton' onclick="removeBook(${i});" href="#" class="btn btn-danger">Remove</a></th></tr>`;
     table.innerHTML += data;
   });
   saveToLocalStorage();
@@ -58,5 +53,5 @@ document.getElementById('submit-button').addEventListener('click', () => {
   const newRead = document.getElementById('read').checked;
   const newBook = new Book(newAuthor, newTitle, newPages, newRead);
   addBookToLibrary(newBook);
-index();
-})
+  index();
+});
